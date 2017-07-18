9f6496b0e5fe211e97d595ef0855023cdd042f50
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-18 16:57:40 +0000

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,10 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end    
+    
+    def angular
+        @angular_port = Portfolio.angular
+    end    
 
     def new 
        @portfolio_item = Portfolio.new 
